8c8d17e56aba986f705f2172690197d4d88752f6
Bareera Nawaz <bareeranawaz1043@gmail.com>
2026-03-24 22:51:17 +0500

    feat: add GET /api/cows to fetch all cows

--- server/controllers/cowcontrollers.js
+++ server/controllers/cowcontrollers.js
@@ -1,11 +1,19 @@
 import Cow from "../models/cow.js";
 
 export const createCow = async (req, res, next) => {
-  try {
-    const { name, age, breed, milkCapacity } = req.body;
-    const cow = await Cow.create({ name, age, breed, milkCapacity });
-    res.status(201).json({ success: true, cow });
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const { name, age, breed, milkCapacity } = req.body;
+        const cow = await Cow.create({ name, age, breed, milkCapacity });
+        res.status(201).json({ success: true, cow });
+    } catch (error) {
+        next(error);
+    }
+};
+export const getCows = async (req, res, next) => {
+    try {
+        const cows = await Cow.find();
+        res.status(200).json({ success: true, cows });
+    } catch (error) {
+        next(error);
+    }
 };
--- server/routes/cowroutes.js
+++ server/routes/cowroutes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { createCow } from "../controllers/cowController.js";
+import { getCows } from "../controllers/cowcontroller.js";
 
 const router = express.Router();
+
+router.get("/", getCows);
 
 router.post("/", createCow);
 
