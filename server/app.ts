dfcfefab0bf0366cfb57efddd111d992b2577d61
Bareera Nawaz <bareeranawaz1043@gmail.com>
2026-03-25 23:54:37 +0500

    Integrated Feeding routes into Express app

--- server/app.ts
+++ server/app.ts
@@ -5,32 +5,29 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.ts";
 import cowRoutes from "./routes/cowroutes.ts";
 import { errorHandler } from "./middleware/errorhandler.ts";
-
-// Load environment variables
+import feedingRoutes from "./routes/feedingroutes";
 dotenv.config();
 
-// Connect to MongoDB
 connectDB();
 
-// Initialize Express app
 const app: Application = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Routes
+
 app.use("/api/cows", cowRoutes);
 
-// Root endpoint
+app.use("/api/feeding", feedingRoutes);
+
 app.get("/", (req: Request, res: Response) => {
   res.send("API is running...");
 });
 
-// Error handling middleware (last)
+
 app.use(errorHandler);
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
