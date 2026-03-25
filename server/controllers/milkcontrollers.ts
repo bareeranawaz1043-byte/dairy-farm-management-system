7c1c7b6d31989a13598299e8331ec925b1eef981
Bareera Nawaz <bareeranawaz1043@gmail.com>
2026-03-25 13:54:58 +0500

    milk controllers skeleton

---
+++ server/controllers/milkcontrollers.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import Milk from "@/models/Milk";
+
+export const createMilk = async (req: Request, res: Response) => {
+  
+};
+
+export const getMilk = async (req: Request, res: Response) => {
+  
+};
