243b35bb67549b6fb2cefe24b8566c6c8d4c40f8
Bareera Nawaz <bareeranawaz1043@gmail.com>
2026-04-01 22:17:10 +0500

    Add auth middleware and protect worker routes

--- server/middleware/authmiddleware.ts
+++ server/middleware/authmiddleware.ts
@@ -1,8 +1,15 @@
+// middleware/authmiddleware.ts
 import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
+// Extend Request type to include user
+interface AuthRequest extends Request {
+  user?: any;
+}
+
+// PROTECT ROUTES
 export const protect = (
-  req: Request,
+  req: AuthRequest,
   res: Response,
   next: NextFunction
 ) => {
@@ -14,11 +21,22 @@ export const protect = (
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-
-    (req as any).user = decoded;
+    req.user = decoded;
 
     next();
   } catch (error) {
     res.status(401).json({ message: "Invalid token" });
   }
+};
+
+// ADMIN ONLY ROUTES
+export const adminOnly = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.user || req.user.role !== "admin") {
+    return res.status(403).json({ message: "Admin only access" });
+  }
+  next();
 };
--- server/routes/userroutes.ts
+++ server/routes/userroutes.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { loginUser,getUsers, deleteUser } from "../controllers/usercontrollers.ts";
+import { loginUser, getUsers, deleteUser } from "../controllers/usercontrollers.ts";
+import { protect, adminOnly } from "../middleware/authmiddleware.ts";
 
 const router = express.Router();
-
-router.get("/", getUsers); 
-router.delete("/:id", deleteUser); 
+router.get("/", protect, adminOnly, getUsers);
+router.delete("/:id", protect, adminOnly, deleteUser);
 router.post("/login", loginUser);
 
 export default router;
