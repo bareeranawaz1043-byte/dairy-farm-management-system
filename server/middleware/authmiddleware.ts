62e2717de42007c9caaccb08680998597642be85
Bareera Nawaz <bareeranawaz1043@gmail.com>
2026-04-05 16:26:43 +0500

    backend connected

--- server/middleware/authmiddleware.ts
+++ server/middleware/authmiddleware.ts
@@ -1,6 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import User from "../models/user";
+import User from "../models/user.ts";
 
 // Extend Request type to include user
 interface AuthRequest extends Request {
