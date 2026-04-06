4f6c4e3d48dbed27244029986a7fc4509cea5ba8
Bareera Nawaz <bareeranawaz1043@gmail.com>
2026-04-06 23:55:57 +0500

    feeding model updated

--- server/models/feeding.ts
+++ server/models/feeding.ts
@@ -1,31 +1,37 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, model } from "mongoose";
 
 interface IFeeding extends Document {
   cow: mongoose.Schema.Types.ObjectId;
   feedType: string;
   quantity: number;
   date: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
-const FeedingSchema = new Schema<IFeeding>({
-  cow: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Cow",
-    required: true,
+const FeedingSchema = new Schema<IFeeding>(
+  {
+    cow: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Cow",
+      required: true,
+    },
+    feedType: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "Quantity cannot be negative"],
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  feedType: {
-    type: String,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    min: [0, "Quantity cannot be negative"],
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true } 
+);
 
-export default mongoose.model<IFeeding>("Feeding", FeedingSchema);
+export default model<IFeeding>("Feeding", FeedingSchema);
