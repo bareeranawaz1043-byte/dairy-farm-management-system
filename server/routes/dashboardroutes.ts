016958dc29fcda4ed51d4ab0f888f74308b61a0b
Bareera Nawaz <bareeranawaz1043@gmail.com>
2026-04-12 23:11:38 +0500

    dashboard routes are changed

--- server/routes/dashboardroutes.ts
+++ server/routes/dashboardroutes.ts
@@ -1,12 +1,21 @@
 import express from "express";
-import { getTotalCows, getTotalMilk, getMonthlyReport } from "../controllers/dashboardcontroller.ts";
+import {
+  getTotalCows,
+  getTotalMilk,
+  getTotalSales,   
+  getAlerts,      
+  getMonthlyReport,
+} from "../controllers/dashboardcontroller.ts"; 
+
 import { protect, adminOnly } from "../middleware/authmiddleware.ts";
 
 const router = express.Router();
 
+
 router.get("/total-cows", getTotalCows);
 router.get("/total-milk", getTotalMilk);
-
+router.get("/total-sales", getTotalSales); 
+router.get("/alerts", getAlerts);          
 router.get("/monthly", protect, adminOnly, getMonthlyReport);
 
 export default router;
